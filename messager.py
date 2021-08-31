0334dbcffe9e545d2ffcd825b63a21347894bb13
saltyKaane <turrutia9@gmail.com>
2021-08-31 13:52:25 -0400

    added python script

---
+++ messager.py
@@ -0,0 +1,30 @@
+import tweepy
+import keys
+
+class MyListener(tweepy.StreamListener):
+    def __init__(self, username):
+        super().__init__()
+        user=username
+        self.myStatus = False
+        self.tweet = ''
+
+    def on_status(self, status): 
+        user=api.get_user(self.user)
+        if status.user.id_str != user.id_str:
+            return
+        user = api.get_user(user)
+
+        recipient_id = user
+        text = 'good tweet!'
+        dm = api.send_direct_message(user, text)
+
+auth = tweepy.OAuthHandler(keys.ckey, keys.csecret)
+auth.set_access_token(keys.atoken, keys.asecret)
+api = tweepy.API(auth)
+
+user = api.get_user('jack')
+
+listener = MyListener(user)
+
+stream = tweepy.Stream(auth, listener)
+stream.filter(follow=[user.id_str], is_async=True)
